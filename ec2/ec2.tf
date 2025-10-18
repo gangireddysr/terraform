bcac857619362402ddc9022cc1816dbc086f2a9b
gangireddysr <gangireddy.sr96@gmail.com>
2025-10-18 13:14:44 +0530

    terraform

---
+++ ec2/ec2.tf
@@ -0,0 +1,35 @@
+resource "aws_instance" "terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = "t3.micro"
+
+    tags = {
+        Name = "terraform"
+        Terraform = "true"
+    }
+}
+
+resource "aws_security_group" "allow_allsc" {
+
+    name = "allow_allsc"
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    name = "allow_allsc"
+  }
+
+}
